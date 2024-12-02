9e21885d437756001ebf7f25dcd1540740e6ca6d
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-12-02 16:42:54 -0500

    feat: add day 1

---
+++ src/day_1/part_2/part_2.py
@@ -0,0 +1,10 @@
+import logging
+
+from template.part_1 import Part1
+
+logger = logging.getLogger(__name__)
+
+
+class Part2(Part1):
+    def __init__(self, input_file: list[str]) -> None:
+        super().__init__(input_file)
