c9bca0a30eeacd25dcd86f36acb19e763c3589f5
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2024-12-04 21:50:32 -0500

    update test template

--- tests/template/test_part_1.py
+++ tests/template/test_part_1.py
@@ -1,6 +1,15 @@
+import pytest
+
 from src.read_file import read_input_file
 from src.template.part_1 import Part1
 
 
-def test_part_1():
-    assert True == True
+@pytest.fixture
+def get_test_input():
+    return read_input_file(r"C:\Users\marke\PycharmProjects\advent-of-code-2024-python\src\day_1\test_input.txt")
+
+
+def test_solution(get_test_input):
+    p1 = Part1(get_test_input)
+
+    sol = p1.solution()
