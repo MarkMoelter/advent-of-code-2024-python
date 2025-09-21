f3a208158f577f4292a43c5074b4026689fb99ef
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2025-09-21 13:23:21 -0400

    test: set test values for day 2:part 2

--- tests/day_2/test_part_2.py
+++ tests/day_2/test_part_2.py
@@ -1,15 +1,17 @@
 import pytest
 
 from src.read_file import read_input_file
-from src.template.part_2 import Part2
+from src.day_2.part_2 import Part2
 
 
 @pytest.fixture
 def get_test_input():
-    return read_input_file(r"C:\Users\marke\PycharmProjects\advent-of-code-2024-python\src\day_1\test_input.txt")
+    return read_input_file(r"C:\Users\marke\PycharmProjects\advent-of-code-2024-python\src\day_2\test_input.txt")
 
 
 def test_solution(get_test_input):
     p2 = Part2(get_test_input)
 
     sol = p2.solution()
+
+    assert sol == 4
