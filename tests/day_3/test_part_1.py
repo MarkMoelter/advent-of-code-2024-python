ebf14561d4c37dbffa2344c58dcf5b33180284ef
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2025-09-21 23:02:22 -0400

    test: add tests for day 3 part 1

--- tests/day_3/test_part_1.py
+++ tests/day_3/test_part_1.py
@@ -1,16 +1,18 @@
 import pytest
 
+from src.day_3.part_1 import Part1
 from src.read_file import read_input_file
-from src.template.part_1 import Part1
 
 
 @pytest.fixture
 def get_test_input():
   return read_input_file(
-      r"C:\Users\marke\PycharmProjects\advent-of-code-2024-python\src\template\test_input.txt")
+      r"C:\Users\marke\PycharmProjects\advent-of-code-2024-python\src\day_3\test_input.txt")
 
 
 def test_solution(get_test_input):
   p1 = Part1(get_test_input)
 
   sol = p1.solution()
+
+  assert sol == 161
