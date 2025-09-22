5b8f9e2e426c345472c1e0789742459cf0872af9
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2025-09-21 23:02:44 -0400

    feat: make tests pass

--- src/day_3/part_1/part_1.py
+++ src/day_3/part_1/part_1.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 logger = logging.getLogger(__name__)
 
@@ -7,5 +8,13 @@ class Part1:
   def __init__(self, input_file: list[str]) -> None:
     self.input_file = input_file
 
-  def solution(self):
-    pass
+  def solution(self) -> int:
+    pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+
+    sol = 0
+
+    for line in self.input_file:
+      for m in re.finditer(pattern, line):
+        sol += int(m.group(1)) * int(m.group(2))
+
+    return sol
