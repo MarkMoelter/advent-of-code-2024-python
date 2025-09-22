43c9b0172c6b06b5c57c5e57164cd12b8bb17ea3
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2025-09-21 22:13:21 -0400

    feat: update is_safe method to make tests pass

--- src/day_2/part_2/part_2.py
+++ src/day_2/part_2/part_2.py
@@ -10,39 +10,49 @@ class Part2(Part1):
     super().__init__(input_file)
 
   @staticmethod
-  def is_safe(report: list[int], tolerance: int = 1) -> bool:
+  def is_safe(report: list[int], tolerance: int = 1, min_differ: int = 1,
+      max_differ: int = 3) -> bool:
     """
     Check if the report is safe.
     :param report: The report to check.
     :param tolerance: The number of bad levels that can be tolerated before failing.
+    :param min_differ: The minimum separation distance between two adjacent values.
+    :param max_differ: The maximum separation distance between two adjacent values.
     :return: A boolean indicating if the report is safe.
     """
-    threshold = 3
-    report_change = []
-    bad_levels = 0
+    sign: str | None = None
+    prev_sign: str | None = None
 
-    # Handle safe condition: adjacent levels differ by 1 to 3
-    for idx, val in enumerate(report):
-      # Skip last element
-      if idx == len(report) - 1:
+    # todo: iter through list, if not safe, pop the unsafe value off and decrement tolerance.
+    #  True if it gets to end of list without returning false or tolerance dropping below 0
+    #  False if tolerance drops below 0
+    for idx, num in enumerate(report):
+
+      # skip first num
+      if idx == 0:
         continue
-      # Handle
-      if bad_levels > tolerance:
+
+      # return false if tolerance falls below 0, this is the fail condition for previous recursive call
+      if tolerance < 0:
         return False
 
-      difference = val - report[idx + 1]
-      if abs(difference) > threshold or difference == 0:
-        bad_levels += 1
+      difference = num - report[idx - 1]
 
-      report_change.append(difference)
+      # set sign for always incrementing and decrementing
+      if difference > 0:
+        sign = "inc"
+      elif difference < 0:
+        sign = "dec"
 
-    # Handle safe condition: levels either all increase or all decrease
-    neg_count, pos_count = 0, 0
-    for num in report_change:
-      if num > 0:
-        pos_count += 1
+      if prev_sign is None:
+        prev_sign = sign
 
-      elif num < 0:
-        neg_count += 1
-    # Return False if both counts are greater than one, True otherwise
-    return not (neg_count > 0 and pos_count > 0)
+      is_acceptable_difference = min_differ <= abs(difference) <= max_differ
+
+      sign_matches_prev_sign = sign == prev_sign
+
+      if not is_acceptable_difference or not sign_matches_prev_sign:
+        report.pop(idx - 1)  # Remove previous value from report
+        return Part2.is_safe(report, tolerance - 1)
+
+    return True
