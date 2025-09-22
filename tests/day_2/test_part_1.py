8a34c23042095e803d96b15e42f1ec2b17f5d7af
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2025-09-21 22:10:26 -0400

    test: add tests for the is_safe method

--- tests/day_2/test_part_1.py
+++ tests/day_2/test_part_1.py
@@ -16,3 +16,17 @@ def test_solution(get_test_input):
   sol = p1.solution()
 
   assert sol == 2
+
+
+@pytest.mark.parametrize("level, expected", [
+  ([7, 6, 4, 2, 1], True),
+  ([1, 2, 7, 8, 9], False),
+  ([9, 7, 6, 2, 1], False),
+  ([1, 3, 2, 4, 5], False),
+  ([8, 6, 4, 4, 1], False),
+  ([1, 3, 6, 7, 9], True),
+])
+def test_report_is_safe(level, expected):
+  p1 = Part1(["not important string"])
+
+  assert p1.is_safe(level) == expected
--- tests/day_2/test_part_2.py
+++ tests/day_2/test_part_2.py
@@ -17,3 +17,18 @@ def test_solution(get_test_input):
   sol = p2.solution()
 
   assert sol == 4
+
+
+@pytest.mark.parametrize("level, expected", [
+  ([7, 6, 4, 2, 1], True),
+  ([1, 2, 7, 8, 9], False),
+  ([9, 7, 6, 2, 1], False),
+  ([1, 3, 2, 4, 5], True),
+  ([8, 6, 4, 4, 1], True),
+  ([1, 3, 6, 7, 9], True),
+  ([1, 4, 1, 4, 1], False),
+])
+def test_report_is_safe(level, expected):
+  p2 = Part2(["not important string"])
+
+  assert p2.is_safe(level) == expected
