cb2e0a503554fa64add3bd2dd6f383e4bd0f3f94
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2026-01-21 23:08:58 -0500

    feat: create solution for part 1

--- src/day_4/part_1/part_1.py
+++ src/day_4/part_1/part_1.py
@@ -7,57 +7,38 @@ class Part1:
   def __init__(self, input_file: list[str], word_to_find: str = "XMAS") -> None:
     self.input_file = input_file
     self.word_to_find = word_to_find.upper()
-    self.word_search_dict = self.create_word_search_dict()
-
-  def create_word_search_dict(self) -> dict[tuple[int, int], str]:
-    out = {}
-
-    for y_coord, row in enumerate(self.input_file):
-      for x_coord, letter in enumerate(row):
-        out[(x_coord, y_coord)] = letter
-
-    return out
-
-  def check_neighbors(self,
-      starting_coordinates: tuple[int, int],
-      letter_to_look_for: str
-  ) -> list[tuple[int, int]]:
-    """
-    Check the 8 letters adjacent to the starting coordinates for a specific letter. Return the coordinates of those specific letters.
-
-    1 2 3\n
-    4 X 5\n
-    6 7 8
-
-    Check the adjacent neighbors of a coordinate for a specific letter.
-    :param starting_coordinates:
-    :param letter_to_look_for:
-    :return: A list of coordinates that contain the letter. List is empty if not found.
-    """
-
-    neighbors = []
-
-    # Cycle through adjacent neighbors
-    for x_modify in range(-1, 2):
-      for y_modify in range(-1, 2):
-        neighbor = starting_coordinates[0] + x_modify, starting_coordinates[
-          1] + y_modify
-
-        is_corner_or_side = neighbor not in self.word_search_dict.keys()
-        is_starting_coordinate = x_modify == 0 and y_modify == 0
-
-        if is_corner_or_side or is_starting_coordinate:
-          continue
-
-        if self.word_search_dict[neighbor] == letter_to_look_for:
-          neighbors.append(neighbor)
-
-    return neighbors
 
   def solution(self):
+    result = 0
 
-    for key, value in self.word_search_dict.items():
-      if value == self.word_to_find[0]:
-        pass
+    # loop through file
+    for r_idx, row in enumerate(self.input_file):
+      for c_idx, letter in enumerate(row):
+        # check if starting letter
+        if letter == self.word_to_find[0]:
+          # get the direction of the
+          for drow in range(-1, 2):
+            for dcol in range(-1, 2):
+              if drow == dcol == 0:
+                continue
+              # Check that direction for the word
+              is_valid = True
+              for i in range(len(self.word_to_find)):
+                r2 = r_idx + drow * i
+                c2 = c_idx + dcol * i
 
-    return 18
+                is_inside = 0 <= r2 < len(
+                    self.input_file) and 0 <= c2 < len(self.input_file[0])
+                if not is_inside:
+                  is_valid = False
+                  break
+
+                is_correct_letter = self.input_file[r2][c2] == \
+                                    self.word_to_find[i]
+                if not is_correct_letter:
+                  is_valid = False
+                  break
+              if is_valid:
+                result += 1
+
+    return result
--- tests/day_4/test_part_1.py
+++ tests/day_4/test_part_1.py
@@ -16,31 +16,3 @@ def test_solution(get_test_input):
   sol = p1.solution()
 
   assert sol == 18
-
-
-@pytest.mark.parametrize(
-    "coordinates, expected_result", [
-      ((0, 0), "M"),
-      ((0, 2), "A"),
-      ((9, 9), "X"),
-    ])
-def test_word_search_dict(get_test_input, coordinates, expected_result):
-  p1 = Part1(get_test_input)
-
-  p1.create_word_search_dict()
-
-  assert p1.word_search_dict[coordinates] == expected_result
-
-
-@pytest.mark.parametrize(
-    "coordinates, letter_to_look_for, expected_result", [
-      pytest.param((1, 1), "M", (2, 0), id="test getting correct coordinates"),
-      pytest.param((0, 0), "S", (1, 1), id="test corners"),
-    ]
-)
-def test_check_neighbors(get_test_input, coordinates, letter_to_look_for,
-    expected_result):
-  p1 = Part1(get_test_input)
-  neighbors = p1.check_neighbors(coordinates, letter_to_look_for)
-
-  assert expected_result in neighbors
